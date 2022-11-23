fc80fb2bfb2c2efbeacabc60006084455624ae7d
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-23 10:38:01 -0500

    try catch

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,37 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace HelloWorld
-
-
+namespace TryAndCatch
 {
     class Program
     {
-
-
         static void Main(string[] args)
-
         {
-            Console.WriteLine(Calculate());
-            Console.Read();
+            Console.WriteLine("Please Enter a number!");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+
+            }
+            catch (FormatException)
+            {
+                throw;
+                //Console.WriteLine("Format Exception, please enter the correct type next time.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow Exception");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Null Arg Exception");
+            }finally
+            {
+                Console.WriteLine("This is called with error or success at the end");
+            }
+
+            Console.ReadKey();
         }
-
-        public static int Calculate()
-        {
-
-            Console.WriteLine("Please enter num1");
-            string num1Input = Console.ReadLine();
-
-            Console.WriteLine("Please enter num2");
-            string num2Input = Console.ReadLine();
-
-            int num1 = int.Parse(num1Input);
-            int num2 = int.Parse(num2Input);
-
-
-            int result = num1 + num2;
-            return result;
-        }
-   
     }
 }
+
+
 
