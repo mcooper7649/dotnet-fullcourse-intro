b239c440ce730f3f7a81085210d828669e05836b
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-23 12:40:11 -0500

    if statement structures

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,116 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OperatorsC
+ namespace HellWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 5;
-            int num2 = 3;
-            int num3;
-
-            //unary operators
-
-            num3 = -num1;
-
-            Console.WriteLine("num3 is {0}", num3);
-
-            bool isSunny = true;
-            Console.WriteLine("Is it Sunny? {0}", !isSunny);
-
-
-            //increment operators
-            int num = 0;
-            num++;
-            Console.WriteLine("Num is {0}", num);
-
-            //post increment operator
-            Console.WriteLine("Num is {0}", num++);
-
-            //pre increment operator
-            Console.WriteLine("Num is {0}", ++num);
-
-            //Decrement operator
-            num--;
-            Console.WriteLine("Num is {0}", num--);
-
-            //Post decrement operator
-            Console.WriteLine("Num is {0}", num--);
-
-
-            //pre increment operator
-            Console.WriteLine("Num is {0}", --num);
-
-
-            int result;
-            result = num1 + num2;
-            Console.WriteLine("result of num1 + num2 is {0}", result);
-
-            result = num1 - num2;
-            Console.WriteLine("result of num1 - num2 is {0}", result);
-
-            result = num1 * num2;
-            Console.WriteLine("result of num1 * num2 is {0}", result);
-
-            result = num1 / num2;
-            Console.WriteLine("result of num1 / num2 is {0}", result);
-
-            result = num1 % num2;
-            Console.WriteLine("result of num1 % num2 is {0}", result);
-
-
-            //relational and type operators
-
-            bool isLower;
-
-            isLower = num1 > num2;
-
-            Console.WriteLine("result of num1 < num2 is {0}", isLower);
+            Console.WriteLine("Whats the temperature like?");
+            string userInput = Console.ReadLine();
 
 
 
-            //equality operator
+            int temperature = int.Parse(userInput);
 
-            bool isEqual;
+            if(temperature < 10)
+            {
+                Console.WriteLine("Take the coat");
+            }
 
-            isEqual = num1 == num2;
+            if (temperature == 10)
+            {
+                Console.WriteLine("It's 10 degree C outside.");
+            }
+            if (temperature > 10 && temperature < 30)
+            {
+                Console.WriteLine("Cozy warm!");
+            }
+            else
+            {
+                Console.WriteLine("It's a day for shorts.");
+            }
 
-            Console.WriteLine("result of num1 == num2 is {0}", isEqual);
-
-            isEqual = num1 != num2;
-            Console.WriteLine("result of num1 != num2 is {0}", isEqual);
-
-
-            //Conditional Operators
-
-            bool isLowerAndSunny;
-            isLowerAndSunny = isLower && isSunny;
-
-            Console.WriteLine("result of isLower && isSunny {0}", isLowerAndSunny);
-
-
-
-            //OR operators
-
-            isLowerAndSunny = isLower || isSunny;
-            Console.WriteLine("Result of isLower || isSunny is{0}", isLowerAndSunny);
-
-            Console.ReadKey();
-
-
-
-
-
-
-
-
+            Console.Read();
         }
     }
 }
-
-
