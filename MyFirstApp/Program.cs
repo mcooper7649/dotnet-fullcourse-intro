cd4cd1afe55ee371d96631f7d423da0cb9731c78
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-24 10:59:39 -0500

    basic classes and class challenge

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,35 +1,19 @@
 ﻿using System;
-namespace BreakContinue
+namespace Classes__Basics
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //break
-            //for(int counter = 0; counter < 10; counter++)
-            //{
-            //    Console.WriteLine(counter);
-            //    if(counter == 3)
-            //    {
-            //        Console.WriteLine("At 3 we stop");
-            //        break;
-            //    }
-            //  Console.Read();
-            //}
+            //Create an object of my class
+            //an  instance of human
+            Human denis = new Human();
+            denis.firstName = "Denis";
+            denis.lastName = "Test";
+            denis.IntroduceMyself();
 
-
-            //continue
-            for (int counter = 0; counter < 10; counter++)
-            {
-                if (counter % 2 == 0)
-                {
-                    Console.WriteLine("Now comes an odd number");
-                    continue;
-                }
-                Console.WriteLine(counter);
-
-            }
-            Console.Read();
+            Human Michael = new Human();
+            Michael.IntroduceMyself();
         }
     }
 }
