398042091d592bbeaf2ca642ec3279b0f1d86222
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-24 10:22:49 -0500

    continue and break

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,66 +1,34 @@
 ﻿using System;
-namespace HelloWorld
+namespace BreakContinue
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            //Regular For Loop
-            //for(int counter = 0; counter < 40; counter+=4)
-            // {
-            //     Console.WriteLine(counter + " is lower than 40");
-            // }
-
-
-            //Challenge - CountOut only Odd Numbers from 0 -20;
-            //for (int counter = 1; counter < 20; counter+=2)
+            //break
+            //for(int counter = 0; counter < 10; counter++)
             //{
-
-            //    Console.WriteLine(counter + " is an Odd number.");
-            //}
-
-            //Console.WriteLine("For loop is done");
-            //Console.Read();
-
-
-            //do while loop
-            //int lenghtOfText = 0;
-            //string wholeText = "";
-            //do
-            //{
-            //    Console.WriteLine("Please enter the name of a friend");
-            //    string nameOfFriend = Console.ReadLine();
-            //    int currentLength = nameOfFriend.Length;
-            //    lenghtOfText += currentLength;
-            //    wholeText += nameOfFriend; 
-            //} while (lenghtOfText < 20);
-            //Console.WriteLine("Thanks that was enough.");
-            //Console.Read();
-
-
-            //while loop
-            //    int counter = 0;
-            //    while (counter < 10)
+            //    Console.WriteLine(counter);
+            //    if(counter == 3)
             //    {
-            //        Console.WriteLine(counter);
-            //        Console.ReadLine();
-            //        counter++;
+            //        Console.WriteLine("At 3 we stop");
+            //        break;
             //    }
-            //    Console.Read();
+            //  Console.Read();
             //}
 
-            string students = "";
-            int numStudents = 0;
 
-            while (students.Equals(""))
+            //continue
+            for (int counter = 0; counter < 10; counter++)
             {
-                Console.WriteLine("Hit Enter if a student has been hopped on the bus. Hit any other key when no more students to board.");
-                students = Console.ReadLine();
-                numStudents++;
-                Console.WriteLine("Current count is {0}", numStudents);
+                if (counter % 2 == 0)
+                {
+                    Console.WriteLine("Now comes an odd number");
+                    continue;
+                }
+                Console.WriteLine(counter);
+
             }
-            Console.WriteLine("{0} people are inside the bus. Press Enter to close the program", numStudents);
             Console.Read();
         }
     }
