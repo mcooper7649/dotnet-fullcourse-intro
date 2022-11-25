d7b9bfe81b750298e7dab344735f4d4a090285c3
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-25 15:23:32 -0500

    complete members

---
+++ MyFirstApp/Members.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Members
+{
+    class Members
+    {
+        //member - private field
+        private string memberName;
+        private string jobTitle;
+        private int salary;
+
+        // member - public field
+        public int age;
+
+
+        //member - property -exposes jobTitle safely - properties start w/ Captital Letter
+
+        public string JobTitle
+        {
+            get
+            {
+                return jobTitle;
+            }
+            set
+            {
+                jobTitle = value;
+            }
+        }
+
+        //public member Method - Can be called from other classes
+        public void Introducing(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine("Hi my name is {0} and my jobTitle is {1}. I'm {2} years old", memberName, jobTitle, age);
+            }
+        }
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine("My salary is {0}", salary);
+        }
+
+        public Members()
+        {
+            age = 30;
+            memberName = "Lucy";
+            salary = 30000;
+            jobTitle = "developer";
+            Console.WriteLine("Object Created");
+        }
+
+        //member - finalizer -destructor
+
+        ~Members()
+        {
+            //cleanup statements
+
+            Console.WriteLine("Deconstuction of Members object");
+            Debug.Write("Deconstruction of Members object");
+        }
+    }
+}
+
--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,25 +1,14 @@
 ﻿using System;
-namespace Properties
+namespace Members
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Box box = new Box(3,4,5);
-            //box.SetLength(4);
-
-            //box.Height = -4;
-            //box.Width = 4;
-            Console.WriteLine("Box's width is " + box.Width);
-            box.Width = 10;
-            Console.WriteLine("Box's width is " + box.Width);
-
-            Console.WriteLine("box's volume is " + box.Volume);
-
-
-
-            box.DisplayInfo();
+            Members member1 = new Members();
+            member1.Introducing(true);
         }
 
     }
+
 }
