c49f0b5fee055e3e550dd4e15c53e0bb4fa25b1c
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-25 15:38:53 -0500

    3 different ways to initialize an Array

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -5,8 +5,31 @@ namespace Members
     {
         static void Main(string[] args)
         {
-            Members member1 = new Members();
-            member1.Introducing(true);
+            int[] grades = new int[5];
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 10;
+            grades[3] = 15;
+            grades[4] = 17;
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+
+            string input = Console.ReadLine();
+            // Assign value to array grades at index 0;
+            grades[0] = int.Parse(input);
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+            //Another way to initialize
+            int[] gradesOfMathStudentsA = { 20, 13, 12, 8, 8 };
+
+            //Third way of initializing an array
+            int[] gradesOfMathStudentB = new int[] { 20, 13, 12, 8, 6 };
+
+
+            Console.WriteLine("length of gradesOfMathStudentA: {0}", gradesOfMathStudentsA.Length);
+            Console.ReadKey();
+
         }
 
     }
